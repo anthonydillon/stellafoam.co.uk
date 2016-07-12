5309ee51a797b1a0f368b49ace39f33e87c93ce2
Anthony Dillon <me@anthonydillon.com>
2016-07-12 12:02:42 +0100

    Finish the kitchen doors

--- doors.php
+++ doors.php
@@ -208,7 +208,7 @@ include("functions.php");
 	}
 
 	.list li {
-		font-size: 16px;
+		font-size: 14px;
 		line-height: 1.3;
 		margin-bottom: 5px;
 	}
@@ -301,7 +301,7 @@ include("functions.php");
 			<div class="clear"></div>
 			<div class="row stripe-grey doors-row" id="bedrooms-vinyl">
 				<h2>Vinyl bedroom doors</h2>
-				<div class="eight-col append-four">
+				<div class="twelve-col">
 					<ul class="list">
 						<li>Complete made to measure service, match any colour shown to your preferred design*</li>
 						<li>Modern tones as well as classic wood finishes with the best Vinyl to MFC matches in the industry</li>
@@ -434,7 +434,7 @@ include("functions.php");
 
 			<div class="row stripe-grey is-hidden doors-row" id="bedrooms-acrylic">
 				<h2>Acrylic bedroom doors</h2>
-				<div class="eight-col append-four">
+				<div class="twelve-col">
 					<ul class="list">
 						<li>Made to measure acrylic doors, 18mm worktops and panels</li>
 						<li>Large selection of colours with matching or two-tone metallic edging</li>
@@ -468,7 +468,7 @@ include("functions.php");
 			</div>
 			<div class="row stripe-grey is-hidden doors-row" id="bedrooms-piece">
 				<h2>5-Piece bedroom doors</h2>
-				<div class="eight-col append-four">
+				<div class="twelve-col">
 					<ul class="list">
 						<li>Made to measure service up to a max height of 2250mm</li>
 						<li>Doors are 22mm thick bar Empire design which is 18mm</li>
@@ -539,7 +539,7 @@ include("functions.php");
 			</div>
 			<div class="row stripe-grey is-hidden doors-row" id="bedroom-veneered">
 				<h2>Veneered bedroom doors</h2>
-				<div class="eight-col append-four">
+				<div class="twelve-col">
 					<ul class="list">
 						<li>Standard bedroom sizes, made to measure not available</li>
 						<li>Matching 18mm panels and accessories available</li>
@@ -568,6 +568,19 @@ include("functions.php");
 
 			<div class="row stripe-grey is-hidden doors-row" id="kitchens-vinyl">
 				<h2>Vinyl Kitchen doors</h2>
+				<div class="twelve-col">
+					<ul class="list">
+						<li>Complete made to measure service, match any colour shown to your preferred design*</li>
+						<li>Modern tones as well as classic wood finishes with the best Vinyl to MFC matches in the industry</li>
+						<li>Matching 18mm or 22mm worktops and panels, along with a huge range of accessories</li>
+						<li>Made to measure heights on curved doors</li>
+						<li>Framed doors supplied with gasket but not glass</li>
+						<li>Vinyl wrapped on highest quality HDF</li>
+						<li>10 year guarantee</li>
+						<li>1-2 week turnaround</li>
+						<li>Samples available upon request</li>
+					</ul>
+				</div>
 				<div class="eight-col">
 					<ul class="door-list">
 						<li class="selected" data-name="Amsterdam"><img src="images/doors/kitchen-vinyl/amsterdam-kitchen.jpg" alt="Amsterdam" style="height:87px" /></li>
@@ -589,7 +602,6 @@ include("functions.php");
 						<li data-name="Europe"><img src="images/doors/kitchen-vinyl/europe-kitchen.jpg" alt="Europe" style="height:87px" /></li>
 						<li data-name="Farmhouse Arch"><img src="images/doors/kitchen-vinyl/farmhouse-arch-kitchen.jpg" alt="Farmhouse Arch" style="height:87px" /></li>
 						<li data-name="Farmhouse Square"><img src="images/doors/kitchen-vinyl/farmhouse-square-kitchen.jpg" alt="Farmhouse Square" style="height:87px" /></li>
-						<li data-name="Florida"><img src="images/doors/kitchen-vinyl/florida-kitchen.jpg" alt="Florida" style="height:87px" /></li>
 						<li data-name="Galaxy"><img src="images/doors/kitchen-vinyl/galaxy-kitchen.jpg" alt="Galaxy" style="height:87px" /></li>
 						<li data-name="Gallo"><img src="images/doors/kitchen-vinyl/gallo-kitchen.jpg" alt="Gallo" style="height:87px" /></li>
 						<li data-name="Glebe"><img src="images/doors/kitchen-vinyl/glebe-kitchen.jpg" alt="Glebe" style="height:87px" /></li>
@@ -667,7 +679,7 @@ include("functions.php");
 				</div>
 
 				<h3>Gloss Colours</h3>
-				<ul class="colour-list twelve-col">
+				<ul class="colour-list eight-col">
 					<li data-name="Gloss Anthracite"><img src="images/doors/gloss-anthracite.jpg" alt="Gloss Anthracite" /></li>
 					<li data-name="Gloss Aubergine"><img src="images/doors/gloss-aubergine.jpg" alt="Gloss Aubergine" /></li>
 					<li data-name="Gloss Beige"><img src="images/doors/gloss-beige.jpg" alt="Gloss Beige" /></li>
@@ -676,15 +688,15 @@ include("functions.php");
 					<li data-name="Gloss Cappuccino"><img src="images/doors/gloss-cappuccino.jpg" alt="Gloss Cappuccino" /></li>
 					<li data-name="Gloss Cashmere"><img src="images/doors/bedroom-acrylic/gloss-cashmere.jpg" alt="Gloss Cashmere" /></li>
 					<li data-name="Gloss Cream"><img src="images/doors/bedroom-acrylic/gloss-cream.jpg" alt="Gloss Cream" /></li>
-					<li data-name="Gloss Dakar"><img src="images/doors/gloss-dakar.jpg" alt="Gloss Dakar" /></li>
+					<li data-name="Gloss Dakar"><img src="images/doors/bedroom-acrylic/gloss-dakar.jpg" alt="Gloss Dakar" /></li>
 					<li data-name="Gloss Ivory"><img src="images/doors/gloss-ivory.jpg" alt="Gloss Ivory" /></li>
 					<li data-name="Gloss Light Grey"><img src="images/doors/bedroom-acrylic/gloss-light-grey.jpg" alt="Gloss Light Grey" /></li>
 					<li data-name="Gloss Mira Cosa"><img src="images/doors/gloss-mira-cosa.jpg" alt="Gloss Mira Cosa" /></li>
 					<li data-name="Gloss Mussel"><img src="images/doors/bedroom-acrylic/gloss-mussell.jpg" alt="Gloss Mussel" /></li>
-					<li data-name="Gloss Red"><img src="images/doors/gloss-red.jpg" alt="Gloss Red" /></li>
+					<li data-name="Gloss Red"><img src="images/doors/bedroom-acrylic/gloss-red.jpg" alt="Gloss Red" /></li>
 					<li data-name="Gloss Silver"><img src="images/doors/gloss-silver.png" alt="Gloss Silver" /></li>
 					<li data-name="Gloss Sky Blue"><img src="images/doors/gloss-sky-blue.png" alt="Gloss Sky Blue" /></li>
-					<li data-name="Gloss Stone Grey"><img src="images/doors/gloss-stone-grey.jpg" alt="Gloss Stone Grey" /></li>
+					<li data-name="Gloss Stone Grey"><img src="images/doors/bedroom-acrylic/gloss-stone-grey.jpg" alt="Gloss Stone Grey" /></li>
 					<li data-name="Gloss Tiepolo"><img src="images/doors/bedroom-acrylic/gloss-tiepolo.jpg" alt="Gloss Tiepolo" /></li>
 					<li data-name="Gloss Walnut"><img src="images/doors/gloss-walnut.jpg" alt="Gloss Walnut" /></li>
 					<li data-name="Gloss White"><img src="images/doors/bedroom-acrylic/gloss-white.jpg" alt="Gloss White" /></li>
@@ -692,6 +704,16 @@ include("functions.php");
 			</div>
 			<div class="row stripe-grey is-hidden doors-row" id="kitchens-acrylic">
 				<h2>Acrylic kitchen doors</h2>
+				<div class="twelve-col">
+					<ul class="list">
+						<li>Made to measure acrylic doors, 18mm worktops and panels</li>
+						<li>Large selection of colours with matching or two-tone metallic edging</li>
+						<li>Metallic reverse</li>
+						<li>10 year guarantee</li>
+						<li>1-2 week turnaround</li>
+						<li>Samples available upon request</li>
+					</ul>
+				</div>
 				<div class="eight-col">
 					<ul class="door-list">
 						<li class="selected" data-name="Gloss Black" style="height:87px"><img src="images/doors/gloss-black.jpg" alt="Gloss Black" style="height:87px" /></li>
@@ -716,6 +738,20 @@ include("functions.php");
 			</div>
 			<div class="row stripe-grey is-hidden doors-row" id="kitchens-piece">
 				<h2>5-Piece kitchen doors</h2>
+				<div class="twelve-col">
+					<ul class="list">
+						<li>Made to measure service up to a max height of 2250mm</li>
+						<li>Doors are 22mm thick bar Empire design which is 18mm</li>
+						<li>Matching 18mm or 22mm worktops and panels, along with a huge range of accessories</li>
+						<li>Curved doors available</li>
+						<li>Framed doors supplied with gasket but not glass</li>
+						<li>Centre bar comes loose so you can pick your ideal height</li>
+						<li>Vinyl wrapped on highest quality HDF</li>
+						<li>10 year guarantee</li>
+						<li>1-2 week turnaround</li>
+						<li>Samples available upon request</li>
+					</ul>
+				</div>
 				<div class="eight-col">
 					<ul class="door-list">
 						<li class="selected" data-name="Empire Pippy Oak" style="height:87px"><img src="images/doors/kitchen-5-piece/empire-pippy-oak.jpg" alt="Empire Pippy Oak" style="height:87px" /></li>
@@ -738,6 +774,21 @@ include("functions.php");
 			</div>
 			<div class="row stripe-grey is-hidden doors-row" id="kitchens-painted">
 				<h2>Painted kitchen doors</h2>
+				<div class="twelve-col">
+					<ul class="list">
+						<li>Made to measure service on Arena, Belair, Finsbury, Hartford &amp; Hyde. Albany (Solid), Greenwich (Solid), Hampton (Solid), Millburn (Solid)  & Luzzi standard kitchen sizes only</li>
+						<li>Only Luzzi available in Gloss Cashmere, Gloss Ivory, Gloss Light Grey &amp; Gloss White (Stock)</li>
+						<li>Only Greenwich &amp; Millburn available lacquered, also available sanded</li>
+						<li>Doors are 22mm thick bar ‘Hyde’ and ‘Belair’ which are 18mm</li>
+						<li>Matching 18mm or 22mm worktops and panels as well as accessories available</li>
+						<li>Curved doors available</li>
+						<li>Framed doors supplied with gasket but not glass</li>
+						<li>Painted on  highest quality HDF or solid wood</li>
+						<li>10 year guarantee</li>
+						<li>2-3 week turnaround</li>
+						<li>Samples available upon request</li>
+					</ul>
+				</div>
 				<div class="eight-col">
 					<ul class="door-list">
 						<li class="selected" data-name="Albany" style="height:87px"><img src="images/doors/kitchen-painted/Albany.jpg" alt="Albany" style="height:87px" /></li>
@@ -747,6 +798,8 @@ include("functions.php");
 						<li data-name="Hampton" style="height:87px"><img src="images/doors/kitchen-painted/Hampton.jpg" alt="Hampton" style="height:87px" /></li>
 						<li data-name="Hartford" style="height:87px"><img src="images/doors/kitchen-painted/Hartford.jpg" alt="Hartford" style="height:87px" /></li>
 						<li data-name="Hyde" style="height:87px"><img src="images/doors/kitchen-painted/Hyde.jpg" alt="Hyde" style="height:87px" /></li>
+						<li data-name="Luzzi" style="height:87px"><img src="images/doors/kitchen-painted/Luzzi.jpg" alt="Luzzi" style="height:87px" /></li>
+						<li data-name="Millburn" style="height:87px"><img src="images/doors/kitchen-painted/Millburn.jpg" alt="Millburn" style="height:87px" /></li>
 					</ul>
 				</div>
 				<div class="two-col align-center">
@@ -757,26 +810,44 @@ include("functions.php");
 					<img src="images/DA.jpg" class="door-colour-image" style="height:242px;" alt="" />
 					<p class="door-colour-name">Dakar</p>
 				</div>
-				<h3>Colours</h3>
-				<ul class="colour-list">
-					<li data-name="Dakar" class="selected"><img src="images/DA.jpg" alt="Dakar" /></li>
-					<li data-name="Denim"><img src="images/doors/denim.png" alt="Denim" /></li>
-					<li data-name="Dust Grey"><img src="images/DG.jpg" alt="Dust Grey" /></li>
-					<li data-name="Graphite"><img src="images/doors/graphite.png" alt="Graphite" /></li>
-					<li data-name="Ice Blue"><img src="images/doors/ice-blue.png" alt="Ice Blue" /></li>
-					<li data-name="Ivory"><img src="images/IV.jpg" alt="Ivory" /></li>
-					<li data-name="Cashmere"><img src="images/CM.jpg" alt="Cashmere" /></li>
-					<li data-name="Light Grey"><img src="images/LG.jpg" alt="Light Grey" /></li>
-					<li data-name="Magnolia"><img src="images/doors/magnolia.png" alt="Magnolia" /></li>
-					<li data-name="Mussel"><img src="images/MU.jpg" alt="Mussel" /></li>
-					<li data-name="Olive"><img src="images/doors/olive.png" alt="Olive" /></li>
-					<li data-name="Stone Grey"><img src="images/SG.jpg" alt="Stone Grey" /></li>
-					<li data-name="White"><img src="images/white.jpg" alt="White" /></li>
-					<li data-name="Natural Oak"><img src="images/NO.jpg" alt="Natural Oak" /></li>
-				</ul>
+				<div class="eight-col">
+					<h3>Colours</h3>
+					<ul class="colour-list">
+						<li data-name="Dakar" class="selected"><img src="images/DA.jpg" alt="Dakar" /></li>
+						<li data-name="Denim"><img src="images/doors/denim.png" alt="Denim" /></li>
+						<li data-name="Dust Grey"><img src="images/DG.jpg" alt="Dust Grey" /></li>
+						<li data-name="Graphite"><img src="images/doors/graphite.png" alt="Graphite" /></li>
+						<li data-name="Ice Blue"><img src="images/doors/ice-blue.png" alt="Ice Blue" /></li>
+						<li data-name="Ivory"><img src="images/IV.jpg" alt="Ivory" /></li>
+						<li data-name="Cashmere"><img src="images/CM.jpg" alt="Cashmere" /></li>
+						<li data-name="Light Grey"><img src="images/LG.jpg" alt="Light Grey" /></li>
+						<li data-name="Magnolia"><img src="images/doors/magnolia.png" alt="Magnolia" /></li>
+						<li data-name="Mussel"><img src="images/MU.jpg" alt="Mussel" /></li>
+						<li data-name="Olive"><img src="images/doors/olive.png" alt="Olive" /></li>
+						<li data-name="Stone Grey"><img src="images/SG.jpg" alt="Stone Grey" /></li>
+						<li data-name="White"><img src="images/white.jpg" alt="White" /></li>
+						<li data-name="Gloss Cashmere"><img src="images/doors/bedroom-acrylic/gloss-cashmere.jpg" alt="Gloss Cashmere" /></li>
+						<li data-name="Gloss Ivory"><img src="images/doors/gloss-ivory.jpg" alt="Gloss Ivory" /></li>
+						<li data-name="Gloss Light Grey"><img src="images/doors/bedroom-acrylic/gloss-light-grey.jpg" alt="Gloss Light Grey" /></li>
+						<li data-name="Gloss White"><img src="images/doors/bedroom-acrylic/gloss-white.jpg" alt="Gloss White" /></li>
+					</ul>
+				</div>
+				<div class="four-col last-col">
+					<img src="images/doors/laquered-colours.png" alt="" />
+				</div>
 			</div>
 			<div class="row stripe-grey is-hidden doors-row" id="kitchens-solid">
 				<h2>Solid kitchen doors</h2>
+				<div class="twelve-col">
+					<ul class="list">
+						<li>Standard kitchen sizes, made to measure not available</li>
+						<li>Matching 18mm panels and accessories available</li>
+						<li>Framed doors supplied with gasket but not glass</li>
+						<li>10 year guarantee</li>
+						<li>2-3 week turnaround</li>
+						<li>Samples available upon request</li>
+					</ul>
+				</div>
 				<div class="eight-col">
 					<ul class="door-list">
 						<li class="selected" data-name="Harvard" style="height:87px"><img src="images/doors/kitchen-solid/Harvard.jpg" alt="Harvard" style="height:87px" /></li>
