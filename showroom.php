4261365b5ac4df60b26528d74e042bd161292b37
Anthony Dillon <me@anthonydillon.com>
2016-12-13 20:59:01 +0000

    Update the showroom page

--- showroom.php
+++ showroom.php
@@ -1,16 +1,16 @@
 <?php
 include("functions.php");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"> 
-<head> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /> 
-	<title>Stellafoam - Showroom</title> 
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Stellafoam - Showroom</title>
 	<?php
 		include  'meta.php';
     ?>
-    
+
     <style type="text/css">
 	#content{
 		padding:20px;
@@ -30,21 +30,68 @@ include("functions.php");
 	dt{
 		background: #fff url(subnav-title-bg.jpg) 0 1px no-repeat;
 	}
+
+	img {
+		max-width: 100%;
+	}
+	.box-grey h3 {
+		text-align: center;
+		padding-top: 10px;
+	}
+	.clear {
+		clear: both;
+	}
+	.row {
+		overflow: hidden;
+	}
+
+	body,
+	html,
+	#content {
+		font-size: 16px;
+	}
+
+	.no-padding-bottom {
+		padding-bottom: 0 !important;
+	}
+
+	.is-hidden {
+		display: none;
+	}
+
+	.eight-col {
+		width: 65% !important;
+	}
+
+	.six-col {
+		width: 46.2% !important;
+	}
+
+	.append-four {
+    margin-right: 34.03509% !important;
+	}
+
+	.twelve-col {
+		display: block;
+		width: 100%;
+		margin-bottom: 20px;
+	}
 	</style>
-	<link rel="stylesheet" type="text/css" href="style.css" media="all" /> 
-	<link rel="stylesheet" type="text/css" href="navigation.css" media="all" /> 
+	<link rel="stylesheet" type="text/css" href="kms-styles.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="navigation.css" media="all" />
 	<link rel="stylesheet" type="text/css" href="typography_core.css" media="all" />
     <?php
 		include  'analytics.php';
     ?>
-</head> 
+</head>
 <body id="build">
-	<div id="pageWrapper"> 
+	<div id="pageWrapper">
         <div style="width:190px;float:right"></div>
         <?php
 			include  'header.php';
     	?>
-    	
+
         <div id="breadcrumb"><a href="/" title="Homepage">Homepage</a> > Showroom
         	<div style="float:right;margin-right:2px;">
              	<?php
@@ -57,46 +104,24 @@ include("functions.php");
                 <a href="/cart.php">View List</a>
         	</div>
         </div>
-        <div id="content">
-			<div id="title" style="margin-top:30px;">Showroom</div>
-        	<div style="text-align:left;padding:20px 40px 40px 40px;">
-                <div style="margin-top:30px;">
-                	<?php
-						$imgDir = "flash/";
-						$images = scandir($imgDir); 
-						$ignore = array( ".", ".." );
-						$imageList = '';
-						$$sep = '';
-						
-						for($i = 0; $i < count($images); $i++) { 
-							if(strpos(strtolower($images[$i]), ".jpg")){
-								$imageList .= $sep.$images[$i]; 
-								$sep = ',';
-							}
-						}  
-                    ?>
-					<div style="float:left;margin-right:30px;">
-                        <object type="application/x-shockwave-flash"  width="450" height="338" data="flash/showroom.swf">
-                        <param name="movie" value="flash/showroom.swf" />
-                        <param name="quality" value="high" />
-                        <param value="transparent" name="wmode"/>
-                        <param name="FlashVars" value="images=<?php echo $imageList ?>" />
-                        </object>
-                    </div>
-					<div style="line-height:200%;">
-                    	<p>Why not visit our showroom. Our friendly staff will be happy to talk through any queries you may have about our products or designs.</p>
-						<p style="padding-top:100px;">We offer board, door and handle samples for our entire range of colours, allowing your clients to order their products with confidence and peace of mind.</p>
+        <main id="content">
+					<h1 id="title" style="margin-top:30px;">Showroom</h1>
+		      <div class="row">
+						<div class="six-col">
+		        	<img src="/images/showroom/stellafoam-showroom-hero.png" alt="" />
+		        </div>
+						<div class="six-col last-col">
+		          <p>Why not visit our showroom. Our friendly staff will be happy to talk through any queries you may have about our products or designs.</p>
+							<p>We offer board, door and handle samples for our entire range of colours, allowing your clients to order their products with confidence and peace of mind.</p>
+						</div>
 					</div>
-				</div>
-			</div>
-			<div style="clear:both;height:30px;"></div>
-		</div> 
+				</main>
        <?php
 			include  'footer.php';
     	?>
     </div>
 	<?php
 		include  'copyright.php';
-	?>									
-</body> 
-</html> 
+	?>
+</body>
+</html>
