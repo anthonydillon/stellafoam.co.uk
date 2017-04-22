2f5e1b6af8ab3c9ee7ce5c81ecad96a2b9498f15
Anthony Dillon <me@anthonydillon.com>
2017-04-22 23:50:43 +0100

    Add the placeholder image for stock images

--- product.php
+++ product.php
@@ -177,10 +177,9 @@ if(isset($_POST["sid"]) && isset($_POST["qty"]) && $_POST["sid"] != '' && $_POST
 
 								echo '<div id="stock" class="product-page-stock">';
 								for ($i = 0; $i < count($stock); $i++) {
-									echo $stock[$i]["Stock_Code"];
 									$placeholder_image = '/images/stock/placeholder.jpg';
 									$image_filename = '/images/stock/'.$stock[$i]["Stock_Code"].'.jpg';
-									if (file_exists($image_filename)) {
+									if (file_exists($_SERVER['DOCUMENT_ROOT'] . $image_filename)) {
 										$image = $image_filename;
 									} else {
 										$image = $placeholder_image;
