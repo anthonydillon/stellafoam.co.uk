33b3c1a2c14fed332d2dad4a34bc96a358a6a031
Anthony Dillon <me@anthonydillon.com>
2017-08-10 21:09:31 +0100

    Add images to KMS - 1 hour

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 
 include("functions.php");
 
-define('s1','293ndjxz9dj564%4£%$%££%£%ids02i09e3j923uicm\iosd;dojdfjidf9e23jioocd');
-define('s2','-bkp$%£Y21mof^£$^sdmiottw%$%£peqc,cr2mi0-crf2j90f4^2nj90erk0-m348n9b');
-define('s3','n"!\d2bfu90g\0-"^%^£-dwd^$%£$uj893gfnh0q4j234njh-cfb-2v1=bv42-bvh349');
-define('s4','h\vc8923j89cjfdjfsdj$%£$£ovfduj53hivfdnhufg[gq=q]=q]=rgn-=qtnq904n82');
-define('sessionlen',20000);
 $loggedIn = false;
 
 $user = array();
--- kitchens-made-simple.php
+++ kitchens-made-simple.php
@@ -78,54 +78,60 @@ if(isset($_POST['username']) && isset($_POST['password'])){
 </head>
 <body id="build">
 	<div id="pageWrapper">
-        <div style="width:190px;float:right"></div>
-        <?php
+    <div style="width:190px;float:right"></div>
+      <?php
 			include '_includes/header.php';
     	?>
-
-        <div id="breadcrumb"><a href="/" title="Homepage">Homepage</a> > Kitchens Made Simple
-        	<div style="float:right;margin-right:2px;">
-             	<?php
+			<div id="breadcrumb"><a href="/" title="Homepage">Homepage</a> > Kitchens Made Simple
+        <div style="float:right;margin-right:2px;">
+          <?php
 					$amount = 0;
 					if(isset($_COOKIE["stellafoamorder"])){
 						$amount = count(explode('|',$_COOKIE["stellafoamorder"]));
 					}
 					echo '<strong>Order List:</strong> '.$amount.' items - ';
-				?>
-                <a href="/cart.php">View basket</a>
-                <?php
-                if(!$loggedIn) {
-                 echo '- <a href="#" class="bread-login">Login</a>';
-                }
-
-				include '_includes/crumbuser.php';
-			?>
-        	</div>
+					?>
+          <a href="/cart.php">View basket</a>
+          <?php
+					if(!$loggedIn) {
+						echo '- <a href="#" class="bread-login">Login</a>';
+					}
+					include '_includes/crumbuser.php';
+					?>
         </div>
-        <div id="content">
-        	<div class="row">
-				<div class="six-col justifyed-text">
-				<h1><img src="/images/homepage/stellafoam-kitchens-made-simple.png" alt="Kitchens Made Simple" /></h1>
-					<p class="intro">Introducing Stellafoam&rsquo;s new quick and easy way to design, price and order kitchens. With free marketing material, CAD software and samples; (along with all the price savings of seperate component ordering) our customers will have the competitive edge they need to win the kitchen game!</p>
+			</div>
+			<div id="content">
+				<div class="row" style="position: relative; overflow: hidden;">
+					<div class="six-col justifyed-text">
+						<h1><img src="/images/homepage/stellafoam-kitchens-made-simple.png" alt="Kitchens Made Simple" /></h1>
+						<p class="intro">Introducing Stellafoam&rsquo;s new quick and easy way to design, price and order kitchens. With free marketing material, CAD software and samples; (along with all the price savings of seperate component ordering) our customers will have the competitive edge they need to win the kitchen game!</p>
+					</div>
+					<div class="six-col last-col">
+						<img src="/images/luzzi-ivory.jpg" alt="Luzzi ivory kitchen" class="kms-hero-image" />
+					</div>
+				</div>
+
+				<?php
+				if($loggedIn) {
+					include '_includes/kms-logged-in.php';
+				} else {
+					include '_includes/kms-logged-out.php';
+				}
+				?>
+
+				<div class="row">
+					<div class="twelve-col">
+						<img src="/images/luzzi-ivory-promo.jpg" alt="Luzzi ivory kitchen" />
+					</div>
 				</div>
 			</div>
-
 			<?php
-			if($loggedIn) {
-				include '_includes/kms-logged-in.php';
-			} else {
-				include '_includes/kms-logged-out.php';
-			}
-		    ?>
-
-		</div>
-        <?php
 			include '_includes/footer.php';
-    	?>
-    </div>
+			?>
+		</div>
 	<?php
-		include '_includes/copyright.php';
-   	?>
-   	<script src="js/js-cookie.js"></script>
+	include '_includes/copyright.php';
+	?>
+	<script src="js/js-cookie.js"></script>
 </body>
 </html>
