911e0948be243ecb416624fe2b0d5c3977acc4e3
Anthony Dillon <me@anthonydillon.com>
2019-03-05 07:04:03 +0000

    Flex the footer

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,8 +1,9 @@
 <div id="footer">
-	<a href="/aboutus.php" title="About Us" style="margin-right:80px;">About Us</a>
-	<a href="/delivery.php" title="Delivery" style="margin-right:80px;">Delivery</a>
-	<a href="/tradecounter.php" title="Trade Center" style="margin-right:80px;">Trade Centre</a>
-	<a href="/showroom.php" title="Our Showroom" style="margin-right:80px;">Our Showroom</a>
-	<a href="/contactus.php" title="Contact Us" style="margin-right:80px;">Contact Us</a>
+	<a href="/aboutus.php" title="About Us">About Us</a>
+	<a href="/delivery.php" title="Delivery">Delivery</a>
+	<a href="/tradecounter.php" title="Trade Center">Trade Centre</a>
+	<a href="/showroom.php" title="Our Showroom">Our Showroom</a>
+	<a href="/contactus.php" title="Contact Us">Contact Us</a>
 	<a href="/docs/Terms-and-Conditions.pdf" title="Terms and Conditions">Terms &amp; Conditions</a>
+	<a href="/docs/Privacy-Statement.pdf" title="Privacy Statement">Privacy Statement</a>
 </div>
